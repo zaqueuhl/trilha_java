1a12169c579c5fe5b9b70ff221d274814ac07199
Zaqueu <zaqueuhlima@gmail.com>
2024-08-20 22:07:56 -0300

    metodos da SmartTv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada ?" + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+    }
+}
